34a06e8dd6f6a9f009e18487441b158853f8af69
Cameliya <nastavasina3682@gmail.com>
2024-12-12 15:22:13 +0300

    добавление hooks

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,26 @@
+import { Reviews } from '../types/types';
+
+export const reviews: Reviews[] = [
+  {
+    'id': '64f25ab7-3e09-40e6-b303-85be5c5d932d',
+    'comment': 'This villa is perfect in every way: the view on mountains and waterfalls, the hot tub and the villa itself. The evening here became a great continuation of our journeys over country.',
+    'date': '2024-11-07T21:00:00.490Z',
+    'rating': 5,
+    'user': {
+      'name': 'Emely',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/avatar/6.jpg',
+      'isPro': true
+    }
+  },
+  {
+    'id': 'f22b5e32-2770-4480-8eb8-adadbb4752ca',
+    'comment': 'Bathed in the nature. Completely unplugged. Unforgettable.',
+    'date': '2024-11-05T21:00:00.490Z',
+    'rating': 4,
+    'user': {
+      'name': 'Max',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/avatar/8.jpg',
+      'isPro': false
+    }
+  }
+];
